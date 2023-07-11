0e2869377254b8fab363834061338eeab1ee0e0b
brianaderer <brian@brianaderer.com>
2023-07-11 14:13:08 -0400

    refactor single-quotations to use template parts

--- wp-content/themes/MAL1.0/single-quotations.php
+++ wp-content/themes/MAL1.0/single-quotations.php
@@ -1,16 +1,6 @@
 <?php get_header() ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <?php $id = get_the_ID(); ?>
-    <?php
-    print_r( get_fields( $id ) );
-    foreach ( get_fields( $id ) as $key => $field ) {
-        ?>
-            <pre>
-                <?php echo( $key. ' : ' . $field ) ?>
-            </pre>
-            <?php
-    }
-    ?>
+    <?php get_template_part('template-parts/quotation', 'single');?>
 <?php endwhile; endif; ?>
 
 <?php get_footer(); ?>
---
+++ wp-content/themes/MAL1.0/template-parts/quotation-single.php
@@ -0,0 +1,2 @@
+<h1>single</h1>
+<?php get_template_part('template-parts/quotation');?>
